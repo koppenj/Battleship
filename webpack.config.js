af2e855d1e4301690d89c7a064708a62bd40e50c
koppenj <joshuwakoppen@gmail.com>
2022-06-15 02:17:39 -0400

    Build Working on structure of page.

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,10 @@ const path = require( 'path' );module.exports = {
               test: /\.js$/,
               exclude: /node_modules/,
               use: 'babel-loader',
+          },
+          {
+              test: /\.css$/,
+              use: [{loader: 'style-loader '}, {loader: 'css-loader'}]
           }
       ]
   }
